ad107f53ed00d08cefa539e63723ec765a5e58bd
ziyu <2661623165@q.com>
2018-11-19 15:50:37 +0800

    测试

--- nodeTest/node1.js
+++ nodeTest/node1.js
@@ -4,6 +4,14 @@ var http = require("http");
 
 http.createServer(function (request,response) {
 	response.writeHead(200,{'Content-type':'text/plain'});
+	switch(request.url){
+		case '/1.html' :
+			response.write('111');
+			break;
+		default:
+			response.write('404');
+			break;
+	}
 	response.end("Hello World!");
 }).listen(8888);
 
